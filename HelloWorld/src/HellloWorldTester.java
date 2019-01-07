7a44e9799d743c99aa2d28c7dc6b260cb626c4f4
Tinoco-Andres-216323560 <andrest@my.yorku.ca>
2019-01-07 17:53:22 -0500

    new hello message
    from "world" to "github"

--- HelloWorld/src/HellloWorldTester.java
+++ HelloWorld/src/HellloWorldTester.java
@@ -3,7 +3,7 @@ public class HellloWorldTester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world");
+		System.out.println("hello github");
 	}
 
 }
